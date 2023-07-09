c9f9d014e9f80f495214d4d6ece39ba4014d15f5
arka9088 <arkapravomajumder@gmail.com>
2023-07-09 12:08:26 +0530

    MCA Project

---
+++ src/main/java/com/arkap/service/impl/TransServiceIMPLEMENT.java
@@ -0,0 +1,39 @@
+package com.arkap.service.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.arkap.service.TransService;
+import com.arkap.utility.DBUtil;
+
+public class TransServiceIMPLEMENT implements TransService {
+
+	@Override
+	public String getUserId(String transId) {
+		String userId = "";
+
+		Connection con = DBUtil.provideConnection();
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+
+			ps = con.prepareStatement("select username from transactions where transid=?");
+
+			ps.setString(1, transId);
+
+			rs = ps.executeQuery();
+
+			if (rs.next())
+				userId = rs.getString(1);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return userId;
+	}
+
+}
